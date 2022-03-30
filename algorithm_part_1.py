fbebaf82b597dd44b948209c3fddfc6276fe120e
kyle.macasinag <kylenmac@verizon.net>
2022-03-30 15:46:33 -0400

    Solved some problems. Created a couple problems, too

--- algorithm_part_1.py
+++ algorithm_part_1.py
@@ -1,34 +1,67 @@
 # Number 1:
 
 # def first_and_last(word):
-#     last_letter = word[len(word)]
-#     all_together = word[0] + word([last_letter])
-#     print(word[0])
+#     first_letter = word[0]
+#     last_letter = word[-1]
+#     together = first_letter + last_letter
+#     print(together)
+#     return together
 
-# first_and_last("Support")
+# first_and_last(input("What would you like for the example?"))
 
 
 # Number 2:
 
-def pbj():
-    for number in range(0,101):
+# def pbj():
+#     for number in range(0,101):
  
-        if number % 5 == 0 and number % 3 == 0:
-            print('Peanutbutter and Jelly')
-        elif number % 3 ==0:
-            print('Peanutbutter')
-        elif number % 5 ==0:
-            print('Jelly')
-        else:
-            print(number)
+#         if number % 5 == 0 and number % 3 == 0:
+#             print('Peanutbutter and Jelly')
+#         elif number % 3 ==0:
+#             print('Peanutbutter')
+#         elif number % 5 ==0:
+#             print('Jelly')
+#         else:
+#             print(number)
+
+
+# pbj()
 
 
 # Number 3:
+# def scrambler(word):
+#     for letter in word:
+#         letter
+    
+#     for letter in word:
+#         letter_list = [letter]
+#         print(letter_list)
+#         indexes = len(word)
+#         print(indexes)
+# pass
 
-def scrambler(word):
-    final_result = ""
-    for letters in word:
-        print(letters)
+# scrambler("devCodeCamp")
 
 
-scrambler("soldaten")
+
+#Number 4:
+
+def search_ingredient(ingredients):
+    for item in ingredients:
+             search = input("What would you like to find today?")
+             print(search)
+             print(item)
+             if search == item:
+                 return item
+             elif search != item:
+                 feedback = input("Apologies, nothing came up. Would you like to try agin? Y/N:")
+                 if feedback == "Y":
+                     print("Let's try again!")
+
+                 elif feedback != "Y":
+                     print("Have a nice day!")
+                     quit
+
+
+   
+search_ingredient()
