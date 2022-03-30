853a893283510bbe9059d445ca7e73a91d599676
kyle.macasinag <kylenmac@verizon.net>
2022-03-30 12:38:33 -0400

    Init

---
+++ algorithm_part_1.py
@@ -0,0 +1,34 @@
+# Number 1:
+
+# def first_and_last(word):
+#     last_letter = word[len(word)]
+#     all_together = word[0] + word([last_letter])
+#     print(word[0])
+
+# first_and_last("Support")
+
+
+# Number 2:
+
+def pbj():
+    for number in range(0,101):
+ 
+        if number % 5 == 0 and number % 3 == 0:
+            print('Peanutbutter and Jelly')
+        elif number % 3 ==0:
+            print('Peanutbutter')
+        elif number % 5 ==0:
+            print('Jelly')
+        else:
+            print(number)
+
+
+# Number 3:
+
+def scrambler(word):
+    final_result = ""
+    for letters in word:
+        print(letters)
+
+
+scrambler("soldaten")
